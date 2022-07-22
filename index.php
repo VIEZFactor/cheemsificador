b4d0a771fe23d401e9dc7e8d26e45102eac96d37
VIEZ Factor <tecvi00@gmail.com>
2022-07-22 18:10:57 -0500

    version 1.0

---
+++ index.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="theme-color" content="#9b9121">
+    <meta name="description" content="Traductor de español a Cheems, para traducir tus textos legales, demandas y cartas dirigidas a presidentes de español a Cheems.">
+    <title>Cheemsificador | Traductor de español a Cheems</title>
+    <link rel="shortcut icon" href="css/favicon/favicon.ico">
+	<link rel="icon" sizes="16x16 32x32 64x64" href="img/favicon/favicon.ico">
+	<link rel="icon" type="image/png" sizes="196x196" href="img/favicon/favicon-192.png">
+	<link rel="icon" type="image/png" sizes="160x160" href="img/favicon/favicon-160.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="img/favicon/favicon-96.png">
+	<link rel="icon" type="image/png" sizes="64x64" href="img/favicon/favicon-64.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16.png">
+	<link rel="apple-touch-icon" href="img/favicon/favicon-57.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="img/favicon/favicon-114.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="img/favicon/favicon-72.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="img/favicon/favicon-144.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="img/favicon/favicon-60.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="img/favicon/favicon-120.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="img/favicon/favicon-76.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="img/favicon/favicon-152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="img/favicon/favicon-180.png">
+	<meta name="msapplication-TileColor" content="#FFFFFF">
+	<meta name="msapplication-TileImage" content="img/favicon/favicon-144.png">
+	<meta name="msapplication-config" content="img/favicon/browserconfig.xml">
+    <link rel="manifest" href="manifest.json">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="js/script.min.js" defer=""></script>
+    
+</head>  
+<body>
+
+    <noscript>
+        <div id="advertencia-nojs">            
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <br>
+                        <h2>
+                            Mem dam amsimemdam qumem mom temgams el JamvamScrimpt emcemdimdom em tum amvemgamdomr.
+                        </h2>
+                        <img src="img/cheems.png" alt="Traductor de español a Cheems" class="img-fluid cheems">
+                        
+                        <h1>
+                            Cheemsificador
+                        </h1>
+                        <p class="version">Ver. 1.0</p>
+                        <p>Traductor de español a idioma/lenguaje cheems</p>
+                        <h5>Por favor, enciende el JavaScript en tu navegador para un correcto funcionamiento.</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </noscript>
+    
+
+
+    <div class="container contenedor-principal">
+        <div class="row">
+            <div class="col-12 text-center">          
+
+                <div class="contenedor-traductor">
+                    <img src="img/cheems.png" alt="Traductor de español a Cheems" class="img-fluid cheems">
+                    <h1>
+                        Cheemsificador
+                    </h1>
+                    <p class="version">Ver. 1.0</p>
+                    <p>Traductor de español a idioma/lenguaje cheems</p>
+                    <h5>Introduce tu texto aquí</h5>
+                    <textarea placeholder="Escrimbem tum temxtom amqumí" class="caja-traductor" name="" id="" rows="5" onkeyup="document.getElementById('cheems').innerText = parseText(event.path[0].value);"></textarea>                    
+                </div>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">  
+                <h5>Tu traducción a cheems aquí:</h5>              
+                <p id="cheems"></p>  
+            </div>
+        </div>
+    </div>  
+    <footer>
+        <div class="container">
+            <div class="row">                
+                <div class="col-12">
+                    <p><i class="fa fa-arrow-circle-up"></i> <i class="fa fa-arrow-circle-up"></i> <i class="fa fa-arrow-circle-down"></i> <i class="fa fa-arrow-circle-down"></i> <i class="fa fa-arrow-circle-left"></i> <i class="fa fa-arrow-circle-right"></i> <i class="fa fa-arrow-circle-left"></i> <i class="fa fa-arrow-circle-right"></i> B A</p>
+                    <p class="historial-cambios" data-toggle="modal" data-target="#modulo-historial">Historial de cambios</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <div class="modals">
+        <div class="modal fade" id="modulo-historial" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="modulo-historialLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modulo-historialLabel">Historial de cambios</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body text-center">
+                    <p><span class="badge badge-pill badge-primary">Ver. 1.0</span> <span class="badge badge-pill badge-dark">11-09-2021</span> <br> Se instaló el módulo en el servidor</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal">Entendido</button>
+                </div>
+                </div>
+            </div>
+        </div>        
+
+        <div class="modal fade" id="videopascua" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                <div class="modal-body">
+                    <video id="video-frame"  width="100%" controls>
+                        <source src="media/ella-es.mp4" type="video/mp4">
+                        Tu navegador no soporta los videos, actualízate papu :vVnvVNv.:V.v.
+                    </video> 
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cierralo papu :v</button>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>        
+        /*------------------------------------------------------------------------------------------------*\
+                    LA MERA NETA DEL PLANETA COMO DICE LA CHAVIZA ASÍ BIEN EN ONDA LA CHAVIZA
+                    SI LE IBA A PONER VARIOS HUEVOS DE PASCUA AL SITIO PERO ME DIO MUCHA 
+                    FLOJERA, YA UN DÍA QUE NO TENGA NADA QUE HACER LE PONGO MÁS PERO 
+                    ES LA 1:30 AM Y TENGO MUCHA FLOJERA ASI QUE BAI
+        \*------------------------------------------------------------------------------------------------*/
+    </script>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <script>
+        $(document).ready(function(){
+            $("#videopascua").on('shown.bs.modal', function(){
+                $('#video-frame').trigger('play');
+            });
+            $("#videopascua").on('hide.bs.modal', function(){
+                $('#video-frame').trigger('pause');
+            });
+        });
+    </script>
+    <script src="js/konami.js"></script>
+    <script>
+    jQuery(document).ready(function() {
+        var easter_egg = new Konami(function() {            
+            $("#videopascua").modal();
+        });
+    });
+    </script>
+
+    
+</body>
+</html>
